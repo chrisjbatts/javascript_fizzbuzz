d0d3bf7b4ba9b038ea155aba3b9b8194ba36bc1f
chrisjbatts <chrisjbatts@gmail.com>
2014-11-24 21:18:53 +0000

    basic functionality built and tested

---
+++ spec/JavabuzzSpec.js
@@ -0,0 +1,37 @@
+describe('Javabuzz', function() {
+
+  var javabuzz;
+
+  beforeEach(function() {
+    javabuzz = new Javabuzz();
+  });
+
+  describe('knows when a number is', function() {
+
+    it('divisible by 15', function() {
+      expect(javabuzz.isDivisibleByFifteen(15)).toEqual(true);
+    })
+
+    it('not divisible by 15', function() {
+      expect(javabuzz.isDivisibleByFifteen(1)).toEqual(false);
+    })
+
+    it('divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(3)).toEqual(true);
+    })
+
+    it('not divisible by 3', function() {
+      expect(javabuzz.isDivisibleByThree(1)).toEqual(false);
+    })
+
+    it('divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(5)).toEqual(true);
+    })
+
+    it('not divisible by 5', function() {
+      expect(javabuzz.isDivisibleByFive(1)).toEqual(false);
+    })
+
+  });
+
+});
